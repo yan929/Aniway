7da44ed9c52ec77c82fd745887b9694a29f7309d
Tiffani <yho777@aucklanduni.ac.nz>
2025-04-21 21:54:46 +1200

    Add default image

--- frontend/src/component/PopularItem/LocDataInfo.jsx
+++ frontend/src/component/PopularItem/LocDataInfo.jsx
@@ -1,6 +1,58 @@
+import { useEffect, useState } from "react";
 function DisplayPopLocInfo({ sectionTitle, locList }) {
+  const [locationImagesList, setlocationImagesList] = useState({});
+  const apiServer = import.meta.env.VITE_BACKEND_API;
 
-    
+  const fectchLocationImage = async () => {
+    const mapImagesList = {};
+    try {
+      await Promise.all(
+        locList.map(async (loc) => {
+          try {
+            console.log("Test loc: ", loc);
+
+            const response = await fetch(apiServer + "/api/gmap/", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({ lat: loc.lat, lng: loc.lng }),
+            });
+
+            const data = await response.json();
+            console.log("Test data: ", data);
+
+            if (data.photo_reference) {
+              const photoRes = await fetch(apiServer + "/api/gmap/photo", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ photo_reference: data.photo_reference }),
+              });
+
+              const blob = await photoRes.blob();
+              const url = URL.createObjectURL(blob);
+
+              if (url) {
+                mapImagesList[loc.id] = url;
+              }
+            }
+          } catch (err) {
+            console.log(err);
+          }
+        })
+      );
+    } catch (error) {
+      console.log(error);
+    }
+    setlocationImagesList(mapImagesList);
+  };
+
+  useEffect(() => {
+    if (locList) {
+      fectchLocationImage();
+    }
+  }, [locList]);
+
   return (
     <>
       <h2 className="text-3xl font-extrabold tracking-wide flex items-center mb-6">
@@ -12,13 +64,11 @@ function DisplayPopLocInfo({ sectionTitle, locList }) {
             key={data.id}
             className="relative bg-white rounded-lg overflow-hidden shadow-md  object-cover transition-transform duration-300 ease-in-out transform hover:scale-105"
           >
-            {/* {data.images?.large && (
-              <img
-                src={data.images.large}
-                alt={data.name}
-                className="w-full h-80 object-cover"
-              />
-            )} */}
+            <img
+              src={locationImagesList[data.id] || "/default-image.jpg"}
+              alt={data.name}
+              className="w-full h-60 object-cover"
+            />
 
             <div className="p-2">
               <h3 className="text-lg font-bold truncate">{data.name}</h3>
