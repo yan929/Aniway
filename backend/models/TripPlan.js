df985e6355705c99c0baa2c6e05b27d76d93f242
Tiffani <yho777@aucklanduni.ac.nz>
2025-04-26 10:58:45 +1200

    Create database schema for trip plan

---
+++ backend/models/TripPlan.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const planSchema = new mongoose.Schema(
+  {
+    title: String,
+    content: [
+      {
+        date: { type: Date, require: true },
+        index: { type: Number, require: true },
+        itinerary: [
+          {
+            gpPlaceId: { type: String, require: true },
+            order: Number,
+            arrivalTime: String,
+            note: String,
+          },
+        ],
+      },
+    ],
+
+    user_id: { type: String, require: true }, // foregin key to user, I don't should I need to add or not
+  },
+  {
+    timestamps: true,
+    collection: "tripPlan",
+  }
+);
+
+module.exports = mongoose.model("TripPlan", planSchema);
