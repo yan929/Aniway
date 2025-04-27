3a56f55810c1ec5327a1019ecc4b0dc6b5fb538f
Tiffani <yho777@aucklanduni.ac.nz>
2025-04-27 17:33:17 +1200

    Add route for GET, PATCH and DELETE specific plan

--- backend/controllers/TPlanController.js
+++ backend/controllers/TPlanController.js
@@ -6,18 +6,17 @@ const getAllPlans = asyncHandler(async (req, res) => {
     // Modify later (need to decide use middleware/ query)
     const userId = "1";
 
-    const tripPlanData = await TripPlan.find({ user_id: userId })
+    const tripPlansData = await TripPlan.find({ user_id: userId })
       .sort({ search_ranking: -1 })
       .select("title content user_id");
 
-    const tripPlans = tripPlanData.map((plan) => ({
+    const tripPlans = tripPlansData.map((plan) => ({
+      id: plan.id,
       title: plan.title,
       content: plan.content,
     }));
 
-    res.json({
-      tripPlans,
-    });
+    res.json(tripPlans);
   } catch (error) {
     console.error("Error fetching trip plan:", error);
     res
@@ -40,4 +39,55 @@ const addPlan = asyncHandler(async (req, res) => {
   }
 });
 
-export { getAllPlans, addPlan };
+const getPlan = asyncHandler(async (req, res) => {
+  try {
+    const tripPlanData = await TripPlan.findById(req.params.id);
+    if (!tripPlanData) {
+      return res.status(400).json({ message: "Trip plan not found" });
+    }
+    const tripPlan = {
+      id: tripPlanData.id,
+      title: tripPlanData.title,
+      content: tripPlanData.content,
+    };
+
+    res.status(200).json(tripPlan);
+  } catch (error) {
+    console.error("Error posting trip plan:", error);
+    res.status(500).json({ message: "Server error while posting a trip plan" });
+  }
+});
+
+// Reference from the code of partialUpdateLocation in LocationController
+const partialUpdatePlan = asyncHandler(async (req, res) => {
+  try {
+    const updatedPlan = await TripPlan.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    if (!updatedPlan)
+      return res.status(404).json({ message: "Trip plan not found" });
+    res.json(updatedPlan);
+  } catch (err) {
+    console.error("Error partially updating plan:", err);
+    res.status(500).json({ error: "Partial update failed" });
+  }
+});
+
+const deletePlan = asyncHandler(async (req, res) => {
+  try {
+    const deletePlan = await TripPlan.findByIdAndDelete(req.params.id);
+
+    if (!deletePlan) {
+      return res.status(404).json({ message: "Plan not found" });
+    }
+
+    res.status(200).json({ message: "Delete successful" });
+  } catch (error) {
+    console.error("Error deleting plan:", err);
+    res.status(500).json({ error: "Delete failed" });
+  }
+});
+
+export { getAllPlans, addPlan, getPlan, partialUpdatePlan, deletePlan };
--- backend/routes/TPlanRoutes.js
+++ backend/routes/TPlanRoutes.js
@@ -1,13 +1,19 @@
 // routes/LocationRoutes.js
 import express from "express";
-import { getAllPlans, addPlan } from "../controllers/TPlanController.js";
+import {
+  getAllPlans,
+  addPlan,
+  getPlan,
+  partialUpdatePlan,
+  deletePlan,
+} from "../controllers/TPlanController.js";
 
 const router = express.Router();
 
 router.get("/", getAllPlans);
 router.post("/", addPlan);
-// router.get("/:id", getPlan);
-// router.patch("/:id", partialUpdatePlan);
-// router.delete("/:id", deletePlan);
+router.get("/:id", getPlan);
+router.patch("/:id", partialUpdatePlan);
+router.delete("/:id", deletePlan);
 
 export default router;
