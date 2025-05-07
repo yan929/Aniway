d215d2fd21e00a6d2947af5346de61579588d43e
nkaw981 <nkaw981@aucklanduni.ac.nz>
2025-05-08 10:48:58 +1200

    added ability to query location by anime name

--- backend/middleware/DatabaseMiddleware.js
+++ backend/middleware/DatabaseMiddleware.js
@@ -1,4 +1,5 @@
 import Anime from "../models/Anime.js";
+import Location from "../models/Location.js";
 
 export class DatabaseMiddleware {
   static async queryForAnimeByName (animeName) {
@@ -22,6 +23,20 @@ export class DatabaseMiddleware {
      */
     const allAnimes = await Anime.find({});
     return new Response(allAnimes, { status: 200 });
+  }
+
+  static async queryForLocationsByAnimeName(animeName) {
+    /*
+    * Queries for all locations associated with an anime.
+    */
+    const locationsByEN = await Location.find({ anime_en_names: animeName })
+    const locationsByCN = await Location.find({ anime_cn_names: animeName })
+
+    if (!(locationsByEN && locationsByCN)) {
+      return new Response(null, { status: 404 });
+    }
+
+    return new Response(locationsByEN ? locationsByEN : locationsByCN, { status: 200 });
   }
 
   // TODO: Add more methods as required
