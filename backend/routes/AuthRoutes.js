c8aa61f26072836a3b5aabb4e3aacc342e7f489c
Tiffani <yho777@aucklanduni.ac.nz>
2025-05-11 08:11:41 +1200

    Add revoke user route and relate frontend logic

--- backend/routes/AuthRoutes.js
+++ backend/routes/AuthRoutes.js
@@ -72,4 +72,44 @@ router.get("/api/logout", (req, res, next) => {
   });
 });
 
+router.post("/api/user/delete", async (req, res) => {
+  console.log("Test req.isAuth: ", req.isAuthenticated());
+
+  if (!req.isAuthenticated()) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const user = req.user;
+  const accessToken = user?.accessToken;
+
+  if (!accessToken) {
+    return res.status(400).json({ message: "Access token is missing" });
+  }
+
+  try {
+    const revokeUrl = `https://oauth2.googleapis.com/revoke?token=${accessToken}`;
+
+    await axios.post(revokeUrl, null, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+    console.log(
+      `Google token successfully revoked for user: ${
+        user.id || user.displayName
+      }`
+    );
+
+    req.logout((err) => {
+      if (err) return res.status(500).send("Logout failed");
+      req.session.destroy(() => {
+        res.clearCookie("connect.sid");
+        res.redirect("http://localhost:5173/login");
+      });
+    });
+  } catch (error) {
+    console.log("Error with revoking process: ", error.message);
+  }
+});
+
 export default router;
--- frontend/src/components/Setting/deleteArea.jsx
+++ frontend/src/components/Setting/deleteArea.jsx
@@ -1,10 +1,34 @@
+import apiClient from "../../util/api";
+import { useNavigate } from "react-router-dom";
+import { useUser } from "../../UserContext";
+
 function DeleteAccountSection() {
+  const navigate = useNavigate();
+  const { userDelete } = useUser();
+
+  const handleUserDelete = async () => {
+    console.log("Test deletion");
+
+    try {
+      const response = await apiClient.post(`/api/user/delete`);
+
+      if (response.status === 200) {
+        userDelete();
+        navigate("/");
+      }
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  };
   return (
     <div className="bg-gray-100 p-6 rounded-md">
       <h2 className="text-xl font-bold mb-1">Delete Account</h2>
       <hr className="border-gray-400 mb-2" />
       <p className="text-sm text-gray-600 mb-4">Remove account from the page</p>
-      <button className="bg-gray-800 text-white font-semibold px-5 py-2 rounded-full hover:bg-gray-900">
+      <button
+        className="bg-gray-800 text-white font-semibold px-5 py-2 rounded-full hover:bg-gray-900"
+        onClick={handleUserDelete}
+      >
         Delete
       </button>
     </div>
