8044a4a2303efa63f0791d04a6db01b726919c1f
Tiffani <yho777@aucklanduni.ac.nz>
2025-05-11 16:38:11 +1200

    Remove useProvider context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from 'react-router-dom';
-import App from './App'
-import { AppContextProvider } from './context/AppContext.jsx'; 
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { AppContextProvider } from "./context/AppContext.jsx";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <UserProvider>
-      <AppContextProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </AppContextProvider>
-    </UserProvider>
+    <AppContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AppContextProvider>
   </React.StrictMode>
 );
